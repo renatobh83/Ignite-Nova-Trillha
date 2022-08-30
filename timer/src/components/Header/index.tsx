f8833b0eb3ca189fa2ebf2b13de5bc5435fac51c
renato <neo_rlm@hotmail.com>
2022-08-30 15:54:50 -0300

    layout

---
+++ timer/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './style'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">Time</a>
+        <a href="/history">History</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
